14aaed93af7f37e4e2592d724c7e831b1cf22d9e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-01 08:42:43 +0000

    Deploy Xia0ba0/Xia0ba0.github.io to github.com/Xia0ba0/Xia0ba0.github.io.git:gh-pages

---
+++ assets/customJS/footer.js
@@ -0,0 +1,8 @@
+var __footer = document.getElementById("footer")
+var __footer_wrapper = document.getElementById("footer-wrap")
+
+
+__footer.style.background = "rgba(0,0,0,0)"
+
+__footer_wrapper.style.fontWeight = "bold"
+__footer_wrapper.style.color = "#534bae"
